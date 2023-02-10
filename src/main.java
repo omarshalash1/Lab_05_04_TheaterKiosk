4afbf1dce950776016df97fad50d099e18321b92
omars <omars@DESKTOP-PURJVSK.lan>
2023-02-10 17:03:39 -0500

    TheaterKiosk

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your age: ");
+    int age = 19;
+    
+    if (age >= 21) {
+      System.out.println("You get a wrist band.");
+    }
+  }
+}
