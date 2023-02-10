2dbb88500f1e6336439e07384f95b9626b2dc16f
omarshalash1 <124627562+omarshalash1@users.noreply.github.com>
2023-02-10 17:07:40 -0500

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.print("Enter your age: ");
-    int age = 19;
+    int age = input.nextInt();
     
     if (age >= 21) {
       System.out.println("You get a wrist band.");
